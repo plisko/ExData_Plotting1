fe5dd69d158655395bcee740481411b2e7f08ca5
plisko <lischetti.paolo@gmail.com>
2014-05-11 10:51:33 +0200

    four plot scripts

--- plot3.R
+++ plot3.R
@@ -1,3 +1,6 @@
+###
+### DATA LOAD ###
+
 setwd("/Users/paul/Documents/Coursera/04_Exploratory Data Analysis/hw/peer_assessment_01/")
 
 pcons_data <- read.table(file="household_power_consumption.txt", header=TRUE, sep=';', stringsAsFactors=FALSE)
@@ -5,13 +8,20 @@ pcons_data <- read.table(file="household_power_consumption.txt", header=TRUE, se
 # verify the correctness of the import
 str(pcons_data)
 
+# create timestamp column
 pcons_data$TimeStamp <- as.POSIXct(
-                            apply(pcons_data[,c("Date", "Time")], 1,
-                                  function(x) paste(x, collapse=' ')),
-                            format='%d/%m/%Y %H:%M:%S')
+    apply(pcons_data[,c("Date", "Time")], 1,
+          function(x) paste(x, collapse=' ')),
+    format='%d/%m/%Y %H:%M:%S')
 
+# keep only data for the selected days
+pcons_data <- pcons_data[(pcons_data$TimeStamp >= as.POSIXct("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S"))
+                         & (pcons_data$TimeStamp < as.POSIXct("03/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S")),]
+
+# remove unused columns
 pcons_data <- pcons_data[,!(names(pcons_data) %in% c("Date","Time"))]
 
+# convert numeric columns
 pcons_data$Global_active_power <- as.numeric(pcons_data$Global_active_power)
 pcons_data$Global_reactive_power <- as.numeric(pcons_data$Global_reactive_power)
 pcons_data$Global_intensity <- as.numeric(pcons_data$Global_intensity)
@@ -24,37 +34,24 @@ pcons_data$Sub_metering_3 <- as.numeric(pcons_data$Sub_metering_3)
 str(pcons_data)
 
 
-pcons_data <- pcons_data[(pcons_data$TimeStamp >= as.POSIXct("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S"))
-                         & (pcons_data$TimeStamp < as.POSIXct("03/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S")),]
+###
+### GENERATING PLOT ###
 
+# GENERATING PLOT ON SCREEN
+plot(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_1,
+     xlab="", ylab="Energy sub metering",
+     type="l",
+     cex.axis=0.8, cex.lab=0.8,
+     col="black")
+lines(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_2,
+      col="red")
+lines(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_3,
+      col="blue")
+legend("topright", col=c("black", "red", "blue"),
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       lwd=1, cex=0.6, y.intersp=0.7)
 
-# FIRST PLOT
-hist(x=pcons_data$Global_active_power,
-     xlab="Global Active Power (kilowatts)",
-     col="red",
-     main="Global Active Power"     
-     )
-
-# SECOND PLOT
-plot(x=pcons_data$TimeStamp, y=pcons_data$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l")
-
-# THIRD PLOT
-plot(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_1, xlab="", ylab="Energy sub metering", type="l", col="black")
-lines(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_2, col="red")
-lines(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_3, col="blue")
-legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=1, cex=0.6)
-
-# FOURTH PLOT
-par(mfcol=c(2,2))
-# upper-left plot
-plot(x=pcons_data$TimeStamp, y=pcons_data$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l")
-# lower-left plot
-plot(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_1, xlab="", ylab="Energy sub metering", type="l", col="black")
-lines(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_2, col="red")
-lines(x=pcons_data$TimeStamp, y=pcons_data$Sub_metering_3, col="blue")
-legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=1, cex=0.5, bty="n")
-# upper-right plot
-plot(x=pcons_data$TimeStamp, y=pcons_data$Voltage, xlab="datetime", ylab="Voltage", type="l", col="black")
-# lower-right plot
-plot(x=pcons_data$TimeStamp, y=pcons_data$Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type="l", col="black")
+# COPYING TO PNG
+dev.copy(png, file="plot3.png", width=480, height=480, units="px")
+dev.off()
 
